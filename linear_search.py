a43357928b8aebd0916ef15d672d2ca9e0317e12
asherorr <asher@mailfence.com>
2022-10-05 12:57:55 -0400

    This commit will add a basic linear search algorithm.

---
+++ linear_search.py
@@ -0,0 +1,21 @@
+def linear_search(lst, target):
+    #Returns the index position of the target if found, else returns None
+    for i in range(0, len(lst)):
+        if lst[i] == target:
+            return i
+    return None
+
+
+def verify(index_value):
+    if index_value is not None:
+        print(f"Target found at {index_value}")
+    else:
+        print("Target not found in list.")
+        
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+result = linear_search(numbers, 12)
+verify(result)
+
+result = linear_search(numbers, 6)
+verify(result)
